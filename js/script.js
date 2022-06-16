640787e37da5a9f7ca15b093d85f1e78b87fbe63
AkromCik <@akromjonrahimjon712>
2022-06-16 21:00:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Javascript Home Work lesson 10
+
+// -------------N1---------------- 
+
+function addition(a, b) {
+    return a + b
+}
+
+// -------------N2---------------- 
+
+
+function convert(minutes) {
+    minutes * 60
+  }
+
+// -------------N3---------------- 
+
+function addition(num) {
+    //  return ++num
+    //  return num + 1
+      let num = num + 1
+    }
+    
+
+// -------------N4---------------- 
+
+function triArea(base, height) {
+    return (base * height) 
+}
+// -------------N5---------------- 
+
+function calcAge(age) {
+    let days = 365;
+    return days * age
+  }
+
+// -------------N6---------------- 
+function remainder(x, y) {
+  return x % y
+}
+// -------------N7---------------- 
+
+function footballPoints(w,d) {
+	return w*3 + d
+}
+
+// -------------N8---------------- 
+
+function points(tP, thP) {
+	return (tP * 2) + (thP * 3)
+}
+
+// -------------N9---------------- 
+
+function lessThan100(a, b) {
+	return a + b < 100 ? true : false
+}
+
+// -------------N10---------------- 
+function animals(chickens, cows, pigs) {
+	return (chickens * 2) + (cows * 4) + (pigs * 4)
+}
